aea5b20f1bb82e9c4bae3d185d38083d79822ec0
Manuel Carrer <manuelca@kjemi.uio.no>
2023-03-28 12:28:49 +0200

    Use gruvbox-flat

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,12 @@
 return {
+  { "eddyekofo94/gruvbox-flat.nvim",
+    init = function()
+      vim.g.gruvbox_transparent = true
+      vim.g.gruvbox_italic_functions = true
+      vim.g.gruvbox_flat_style = "dark"
+      vim.g.gruvbox_theme = { Conceal = { fg = "#d4be98" } }
+    end,
+  },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
