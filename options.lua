89d40b58872bec12743a1c88d8d7a339714c2be9
Manuel Carrer <manuelca@kjemi.uio.no>
2023-04-13 16:19:39 +0200

    Change options

--- options.lua
+++ options.lua
@@ -7,6 +7,8 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    termguicolors = true,
+    cursorlineopt = "number",
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
@@ -17,6 +19,9 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
+  o = {
+    background = "dark",
+  }
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
