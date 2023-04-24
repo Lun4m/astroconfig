717cdaa52fcde1b2e2ae2628d1d63c609b66e521
Manuel Carrer <manuelca@kjemi.uio.no>
2023-04-24 10:38:13 +0200

    Remove unused coloroschemes

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,15 +4,15 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.everforest" },
-  { import = "astrocommunity.colorscheme.gruvbox" },
-  { import = "astrocommunity.colorscheme.mini-base16" },
-  { import = "astrocommunity.colorscheme.nightfox" },
-  { import = "astrocommunity.colorscheme.onigiri" },
-  { import = "astrocommunity.colorscheme.oxocarbon" },
-  { import = "astrocommunity.colorscheme.rose-pine" },
-  { import = "astrocommunity.colorscheme.tokyonight" },
-  { import = "astrocommunity.colorscheme.kanagawa" },
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.everforest" },
+  -- { import = "astrocommunity.colorscheme.gruvbox" },
+  -- { import = "astrocommunity.colorscheme.mini-base16" },
+  -- { import = "astrocommunity.colorscheme.nightfox" },
+  -- { import = "astrocommunity.colorscheme.onigiri" },
+  -- { import = "astrocommunity.colorscheme.oxocarbon" },
+  -- { import = "astrocommunity.colorscheme.rose-pine" },
+  -- { import = "astrocommunity.colorscheme.tokyonight" },
+  -- { import = "astrocommunity.colorscheme.kanagawa" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
