7e218c7d927319800381a7b8981476bf133e90af
Manuel Carrer <manuelca@kjemi.uio.no>
2023-06-02 21:27:45 +0200

    Fix vimtex

--- plugins/user.lua
+++ plugins/user.lua
@@ -23,14 +23,19 @@ return {
   },
   {
     'lervag/vimtex',
-    init = function()
+    config = function()
+      vim.o.textwidth = 100
+      vim.o.wrap = true
+      vim.o.conceallevel = 2
+      vim.o.cmdheight = 1
       vim.g.vimtex_compiler_method = "latexmk"
       -- vim.g.vimtex_view_method = "general"
       vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_quickfix_mode = 0
       vim.g.vimtex_view_forward_search_on_start = 0
     end,
-    event = "BufRead",
+    -- event = "BufRead",
+    ft = { "tex", "bib" }
   },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
