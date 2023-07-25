a042cc2a6432bbd3798abcf6a92c3db105f1ae7e
Manuel Carrer <manuelca@kjemi.uio.no>
2023-07-25 11:16:31 +0200

    Remove tab for snippets

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -4,26 +4,52 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
       local luasnip = require("luasnip")
+
+      local function has_words_before()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
       opts.mapping["<C-j>"] = cmp.mapping(
-          function(fallback)
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end,
-          { "i", "s" }
+        function(fallback)
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+        { "i", "s" }
       )
+
       opts.mapping["<C-k>"] = cmp.mapping(
-          function(fallback)
-            if luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end,
-          { "i" , "s"}
+        function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+        { "i", "s" }
       )
+
+      opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      opts.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
       return opts
     end,
   },
